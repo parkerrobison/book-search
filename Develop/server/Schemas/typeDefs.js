6e00626109e2fa76cd55bb06eeddf23ad62a287a
probison <parker.robison@hotmail.com>
2020-09-20 15:52:57 -0600

    added resolvers

--- Develop/server/Schemas/typeDefs.js
+++ Develop/server/Schemas/typeDefs.js
@@ -1,9 +1,27 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-    type Query {
-        helloWorld: String
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }    
+
+    type Book {
+        bookId: String
+        authors: [String]
+        description: String
+        image: String
+        link: String
+        title: String
     }
+    type Query {
+        user(username: String!): User
+        books: [Book]
+    }
+
 `;
 
 module.exports = typeDefs;
